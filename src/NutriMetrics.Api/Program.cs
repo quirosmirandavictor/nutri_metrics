43e8d6d0c496ff5d1e7c77b88540ef73d8d20bf9
Victor Quirós Miranda <quirosmirandavictor@gmail.com>
2026-07-22 23:05:18 -0600

    nutri_metrics: Add swagger
    Add swagger UI with Authorize button

--- src/NutriMetrics.Api/Program.cs
+++ src/NutriMetrics.Api/Program.cs
@@ -1,11 +1,29 @@
 using NutriMetrics.Modules.CalorieTracking.Infrastructure;
 using NutriMetrics.Modules.Identity.Infrastructure;
+using Microsoft.OpenApi; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // --- DI Container ---
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+{
+    document.Components ??= new OpenApiComponents();
+    document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>(); // ← agregar esta línea
+
+    document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Ingresá tu JWT token (sin 'Bearer ' delante)"
+    };
+    return Task.CompletedTask;
+});
+});
 
 // Modules Registration
 builder.Services.AddIdentityModule(builder.Configuration);
@@ -17,6 +35,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+     app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "NutriMetrics API v1");
+        options.RoutePrefix = "swagger";
+    });
+    
 }
 
 app.UseHttpsRedirection();
